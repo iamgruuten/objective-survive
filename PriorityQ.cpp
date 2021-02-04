5de5f5ac5c712bcbb61fe3a784930a1036db9c1f
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2021-02-04 22:44:33 +0800

    Partial done of PQ

--- PriorityQ.cpp
+++ PriorityQ.cpp
@@ -1,110 +1,75 @@
-#include<string>
-#include<iostream>
 #include "PriorityQ.h"
 
-template <typename T>
-void PriorityQ<T>::swap(int *a, int *b){
-    int c;
-    c = *a;
-    *a = *b;
-    *b = c;
+template<class T> HeapPriorityQueue<T>::HeapPriorityQueue(bool (*comp)(T obj1, T obj2))
+{
+    this -> compare = comp;
 }
-template <typename T>
-int PriorityQ<T>::get_right_child(int ArrayQ[], int index){
-    if((((2*index)+1) < array_size) && (index >= 1))
+
+template<class T> bool HeapPriorityQueue<T>::isEmpty()
+{
+    return heap.empty();
+}
+
+template<class T> void HeapPriorityQueue<T>::insert(T obj)
+{
+    heap.push(obj);
+    heapifyUp(heap.size()-1);
+}
+
+template<class T> void HeapPriorityQueue<T>::pop()
+{
+    if(!isEmpty())
     {
-        return (2*index)+1;
+       std::swap(heap[0], heap[heap.size()-1]);
+       heap.pop_back();
+       if(!isEmpty())
+          heapifyDown(0);
     }
-    return -1;
 }
-template <typename T>
-int PriorityQ<T>::get_left_child(int ArrayQ[], int index){
-    if(((2*index) < array_size) && (index >= 1))
+
+//TODO Search Implementation
+//TODO 
+
+template<class T> T HeapPriorityQueue<T>::getTop()
+{
+    if(!isEmpty())
     {
-        return 2*index;
-    }
-    return -1;
-}
-template <typename T>
-int PriorityQ<T>::get_parent(int ArrayQ[], int index){
-    if ((index > 1) && (index < array_size)) {
-        return index/2;
-    }
-    return -1;
-}
-template <typename T>
-void PriorityQ<T>::max_heapify(int ArrayQ[], int index){
-    int left = get_left_child(ArrayQ, index);
-    int right = get_right_child(ArrayQ, index);
-
-    int max = index;
-
-    if((left <= heap_size) && (left >0)){
-        if(ArrayQ[left] > ArrayQ[max]){
-            max = left;
-        }
-    }
-
-    if((right <= heap_size && (right > 0))){
-        if((ArrayQ[right] > ArrayQ[max])){
-            max = right;
-        }
-    }
-
-    if(max != index){
-      swap(&ArrayQ[index], &ArrayQ[max]);
-      max_heapify(ArrayQ, max);  
-    }
-}
-template <typename T>
-void PriorityQ<T>::build_max_heap(int ArrayQ[]){
-
-  for(int i=heap_size/2; i>=1; i--) {
-    max_heapify(ArrayQ, i);
-  }
-}
-template <typename T>
-int PriorityQ<T>::maximum(int ArrayQ[]){
-    return ArrayQ[1];
-}
-template <typename T>
-int PriorityQ<T>::get_max(int ArrayQ[]){
-    int maxm = ArrayQ[1];
-    ArrayQ[1] = ArrayQ[heap_size];
-
-    heap_size = heap_size - 1;
-    max_heapify(ArrayQ, 1);
-    return maxm;
-}
-
-template <typename T>
-void PriorityQ<T>::increase_key(int ArrayQ[], int index, T key){
-    ArrayQ[index] = key;
-    while((index > 1) && (ArrayQ[get_parent(ArrayQ, index)] < ArrayQ[index])) {
-        swap(&ArrayQ[index], &ArrayQ[get_parent(ArrayQ, index)]);
-        index = get_parent(ArrayQ, index);
+       return heap[0];
     }
 }
 
-template <typename T>
-void PriorityQ<T>::decrease_key(int ArrayQ[], int index, T key){
-    ArrayQ[index] = key;
-    max_heapify(ArrayQ, index);
+template<class T> void HeapPriorityQueue<T>::heapifyDown(int idx)
+{
+    int largeIdx = idx;
+    int leftChildIdx = leftChild(idx);
+    int rightChildIdx = rightChild(idx);
+    
+    if(leftChildIdx < heap.size())
+    {
+       if(compare(heap[largeIdx], heap[leftChildIdx]))
+            largeIdx = leftChildIdx;
+    }
+    if(rightChildIdx < heap.size())
+    {
+       if(compare(heap[largeIdx], heap[rightChildIdx]))
+            largeIdx = rightChildIdx;
+    }
 
+    if(largeIdx != idx)
+    {
+       std::swap(heap[largeIdx], heap[idx]);
+       heapifyDown(largeIdx);
+    }
 }
 
-template <typename T>
-void PriorityQ<T>::insert(int ArrayQ[], T key){
-    heap_size = heap_size + 1;
-    ArrayQ[heap_size] = -1*INF;
-    increase_key(ArrayQ, heap_size, key);
-}
-
-template <typename T>
-void PriorityQ<T>::print_heap(int ArrayQ[]){
-
-  for(int i = 1; i <= heap_size; i++) {
-    printf("%d\n",ArrayQ[i]);
-  }
-  printf("\n");
+template<class T> void HeapPriorityQueue<T>::heapifyUp(int idx)
+{
+    int parentIdx = parent(idx);
+    if(parentIdx < 0)
+            return;
+    if(compare(heap[parentIdx], heap[idx]))
+    {
+        std::swap(heap[parentIdx], heap[idx]);
+        heapifyUp(parentIdx);
+    }
 }
--- PriorityQ.h
+++ PriorityQ.h
@@ -1,33 +1,39 @@
-#ifndef varray_h
-#define varray_h
+#ifndef MAXHEAP_H
+#define MAXHEAP_H
 
-typedef int ItemType;
-template <typename T>
-class PriorityQ {
+#include<VArray.h>
 
-private:
-    int array_size;
-    int heap_size;
-    const int INF;
-    T* keys;
-
-
-public:
-PriorityQ() : array_size(20), heap_size(0), INF(1000000) {}
-void swap(int *a, int *b){}
-int get_right_child(int ArrayQ[], int index){}
-int get_left_child(int ArrayQ[], int index){}
-int get_parent(int ArrayQ[], int index){}
-void max_heapify(int ArrayQ[], int index){}
-void build_max_heap(int ArrayQ[]){}
-int maximum(int ArrayQ[]){}
-int get_max(int ArrayQ[]){}
-
-void increase_key(int ArrayQ[], int index, T key){}
-void decrease_key(int ArrayQ[], int index, T key){}
-void insert(int ArrayQ[], T key){}
-void print_heap(int ArrayQ[]){}
-
+template<class T>
+class HeapPriorityQueue
+{
+    public:
+        HeapPriorityQueue<T> (bool (*comp)(T obj1, T obj2));
+        //return true if the Max Heap is empty, true otherwise.
+        bool isEmpty();
+        //used to insert an item in the priority queue.
+        void insert(T obj);
+        //deletes the highest priority item currently in the queue.
+        void remove();
+        //return the highest priority item currently in the queue.
+        T getTop();
+    private:
+        //return true if obj1 compares less than obj2
+        bool (*compare)(T obj1, T obj2);
+        VArray<T> heap;
+        void heapifyUp(int idx);
+        void heapifyDown(int idx);
+        int leftChild(int i)
+        {
+            return 2*i + 1;
+        }
+        int rightChild(int i)
+        {
+            return 2*i + 2;
+        }
+        int parent(int i)
+        {
+            return (i-1)/2;
+        }
 };
 
 #endif
