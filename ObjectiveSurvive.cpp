68ad16fe4389770022fa03eb1b12df2e88fae999
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2021-02-07 08:04:19 +0800

    Fixed error

--- ObjectiveSurvive.cpp
+++ ObjectiveSurvive.cpp
@@ -4,26 +4,11 @@
 #include "Game.h"
 
 int main() {
-    Game game = Game();
-    game.start();
-//#include "Game.h"
-#include "Board.h"
-#include "Vec2D.h"
-#include "VArray.cpp"
-#include "EntityClasses.h"
-#include <iostream>
-#include "CSVParser.h"
-#include "FSMStack.h"
-
-int main() {
     
-    Board b = *CSVParser::parseMapFile("b.csv");
     //Melee* m = new Melee();
     //b.spawnEntityCopyAt(Vec2D(4, 4), m);
-    b.display(); 
-
-    e->runState();
-
+    Game game = Game();
+    game.start();
     //Game game = Game();
     //game.start();
     return 0;
